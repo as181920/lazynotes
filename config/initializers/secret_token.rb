620edc0ddd2cbc02e59f940ef9677740b5d731a6
Andersen Fan <as181920@hotmail.com>
2012-12-14 17:50:56 +0800

    init lazynotes rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lazynotes::Application.config.secret_token = 'ab32bc581704fe72b12e7aa7a87317bedcc6d0f5fe026bbaeafea6b95a4c5e45c3da8ea6ef98ec4a5d74cb58f488425c8aaddcc75f998eacb67aaeb36b7eca4f'
